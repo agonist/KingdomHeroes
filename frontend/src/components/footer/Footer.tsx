2aa6941e0cbd67b7c8f6923f2f26acb4e42f3b58
bas <bastien.billey@gmail.com>
2022-02-16 13:32:54 +0800

    setup different sections

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -10,13 +10,13 @@ function Footer() {
     return (
         <Stack direction="row" justifyContent="center" height="100%"
                alignItems="center" paddingY={3} spacing={3} sx={{backgroundColor: "#f1f1f1"}}>
-            <a href={"https://twitter.com/KingdomHeroesN1"} target="_blank">
+            <a href={"https://twitter.com/KingdomHeroesN1"} target="_blank" rel="noreferrer">
                 <Image src={twitter} width={42} height={42}/>
             </a>
-            <a href={"https://twitter.com/KingdomHeroesN1"} target="_blank">
+            <a href={"https://twitter.com/KingdomHeroesN1"} target="_blank" rel="noreferrer">
                 <Image src={discord} width={42} height={42}/>
             </a>
-            <a href={"https://twitter.com/KingdomHeroesN1"} target="_blank">
+            <a href={"https://twitter.com/KingdomHeroesN1"} target="_blank" rel="noreferrer">
                 <Image src={opensea} width={42} height={42}/>
             </a>
         </Stack>
