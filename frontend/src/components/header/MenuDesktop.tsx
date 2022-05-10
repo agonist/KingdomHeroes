8ae31c303dc8a6318e64fda91f1ee794fd00e740
bas <bastien.billey@gmail.com>
2022-05-10 17:05:54 +0800

    assets migration

--- frontend/src/components/header/MenuDesktop.tsx
+++ frontend/src/components/header/MenuDesktop.tsx
@@ -30,65 +30,63 @@ function MenuDesktop() {
     const router = useRouter();
 
     return (
-        <Container>
 
-            <Stack alignItems="center" justifyItems="center">
-                <Stack
-                    direction="row"
-                    alignItems="center"
-                    justifyItems="center"
-                >
-                    <Stack width={160} alignItems={"center"}>
-                        <Typography variant="h4" color={'white'} component={ScrollLink} to={"/"} spy
-                                    smooth offset={-100} sx={{
-                            cursor: "pointer",
-                            textTransform: "uppercase",
-                            textShadow: "-1px 1px 4px  #000000"
-                        }}>
-                            GAME
-                        </Typography>
-                    </Stack>
-                    <Stack width={160} alignItems={"center"}>
-                        <Typography variant="h4" color={'white'} component={ScrollLink} to={"/concept"} spy
-                                    smooth offset={-100} sx={{
-                            cursor: "pointer",
-                            textTransform: "uppercase",
-                            textShadow: "-1px 1px 4px  #000000"
+        <Stack
+            width={"100%"}
+            direction="row"
+            alignItems="center"
+            justifyItems="center"
+            justifyContent={"center"}
+        >
+            <Stack width={160} alignItems={"center"}>
+                <Typography variant="h4" color={'white'} component={ScrollLink} to={"/"} spy
+                            smooth offset={-100} sx={{
+                    cursor: "pointer",
+                    textTransform: "uppercase",
+                    textShadow: "-1px 1px 4px  #000000"
+                }}>
+                    GAME
+                </Typography>
+            </Stack>
+            <Stack width={160} alignItems={"center"}>
+                <Typography variant="h4" color={'white'} component={ScrollLink} to={"/concept"} spy
+                            smooth offset={-100} sx={{
+                    cursor: "pointer",
+                    textTransform: "uppercase",
+                    textShadow: "-1px 1px 4px  #000000"
 
-                        }}>
-                            NFT
-                        </Typography>
-                    </Stack>
-
-                    <Stack maxWidth={170}>
-                        <img src={"crown.png"}/>
-                    </Stack>
-
-                    <Stack width={160} alignItems={"center"}>
-                        <Typography variant="h4" color={'white'} component={ScrollLink} to={"/roadmap"} spy
-                                    smooth offset={-100} sx={{
-                            cursor: "pointer",
-                            textTransform: "uppercase",
-                            textShadow: "-1px 1px 4px  #000000"
-                        }}>
-                            ROADMAP
-                        </Typography>
-                    </Stack>
-                    <Stack width={160} alignItems={"center"}>
-                        <Typography variant="h4" color={'white'} component={ScrollLink} to={"/team"} spy
-                                    smooth offset={-100} sx={{
-                            cursor: "pointer",
-                            textTransform: "uppercase",
-                            textShadow: "-1px 1px 4px  #000000"
-                        }}>
-                            TEAM
-                        </Typography>
-                    </Stack>
-                </Stack>
+                }}>
+                    NFT
+                </Typography>
             </Stack>
 
+            <Stack maxWidth={170}>
+                <img src={"crown.png"}/>
+            </Stack>
 
-        </Container>
+            <Stack width={160} alignItems={"center"}>
+                <Typography variant="h4" color={'white'} component={ScrollLink} to={"/roadmap"} spy
+                            smooth offset={-100} sx={{
+                    cursor: "pointer",
+                    textTransform: "uppercase",
+                    textShadow: "-1px 1px 4px  #000000"
+                }}>
+                    ROADMAP
+                </Typography>
+            </Stack>
+            <Stack width={160} alignItems={"center"}>
+                <Typography variant="h4" color={'white'} component={ScrollLink} to={"/team"} spy
+                            smooth offset={-100} sx={{
+                    cursor: "pointer",
+                    textTransform: "uppercase",
+                    textShadow: "-1px 1px 4px  #000000"
+                }}>
+                    TEAM
+                </Typography>
+            </Stack>
+        </Stack>
+
+
     )
 
 }
